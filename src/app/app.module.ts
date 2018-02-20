a7db32a39cef6a4b0a92acc5e2d958b91239ca4c
Edmundo Elizondo <edmundo.geg@hotmail.com>
2018-02-20 01:52:31 -0600

    feat(app): Add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { GraphQLModule } from './apollo.config';
+import { AppRoutingModule } from './app.routing';
 
 
 import { AppComponent } from './app.component';
 import { LinkItemComponent } from './link-item/link-item.component';
 import { LinkListComponent } from './link-list/link-list.component';
 import { CreateLinkComponent } from './create-link/create-link.component';
+// import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
@@ -16,7 +18,8 @@ import { CreateLinkComponent } from './create-link/create-link.component';
     AppComponent,
     LinkItemComponent,
     LinkListComponent,
-    CreateLinkComponent
+    CreateLinkComponent,
+    // HeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -24,6 +27,8 @@ import { CreateLinkComponent } from './create-link/create-link.component';
 
     // GraphQL connection
     GraphQLModule,
+
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routing.ts
@@ -0,0 +1,40 @@
+/**
+ * Created by Edmundo Elizondo on 2/20/2018.
+ */
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+
+// 1
+import {LinkListComponent} from './link-list/link-list.component';
+import {CreateLinkComponent} from './create-link/create-link.component';
+
+/**
+ * Setup all routes here
+ */
+const routes: Routes = [
+  // 2
+  {
+    path: '',
+    component: LinkListComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'create',
+    component: CreateLinkComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  }
+];
+
+@NgModule({
+  imports: [
+    // 3
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
