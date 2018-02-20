d79ece84d84e453eec19eb32164f87f36f6cbd0c
Edmundo Elizondo <edmundo.geg@hotmail.com>
2018-02-20 02:29:49 -0600

    feat(link): Add store and route update on Link creation

--- src/app/create-link/create-link.component.ts
+++ src/app/create-link/create-link.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Apollo } from 'apollo-angular';
 
-import { CREATE_LINK_MUTATION, CreateLinkMutationResponse } from '../graphql';
+import { ALL_LINKS_QUERY, CREATE_LINK_MUTATION, CreateLinkMutationResponse } from '../graphql';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-link',
@@ -13,7 +14,7 @@ export class CreateLinkComponent implements OnInit {
   description: string = '';
   url: string = '';
 
-  constructor(private apollo: Apollo) { }
+  constructor(private apollo: Apollo, private router: Router) { }
 
   ngOnInit() {
   }
@@ -24,9 +25,26 @@ export class CreateLinkComponent implements OnInit {
       variables: {
         description: this.description,
         url: this.url,
-      }
+      },
+
+      // NOTE: Tutorial doesn't explains too much about this, but here he does:
+      //  https://dev-blog.apollodata.com/tutorial-graphql-mutations-optimistic-ui-and-store-updates-f7b6b66bf0e2
+      //  Also note that we have "optimisticResponse" property to handle optimistic UI.
+      update: (store, {data: {createLink}}) => {
+        // Read the data from the cache for this query.
+        const data: any = store.readQuery({
+          query: ALL_LINKS_QUERY
+        });
+
+        // Add our link from the mutation to the end.
+        data.allLinks.push(createLink);
+
+        // Write the data back to the cache.
+        store.writeQuery({ query: ALL_LINKS_QUERY, data })
+      },
     }).subscribe((response) => {
       console.log('mutation response:', response);
+      this.router.navigate(['/']);
     });
   }
 
