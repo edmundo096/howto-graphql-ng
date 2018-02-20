d81691a216af9762cec4a0f40fbbb48267c7b4f5
Edmundo Elizondo <edmundo.geg@hotmail.com>
2018-02-20 01:35:36 -0600

    feat(link): Add Link creation component and mutation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { GraphQLModule } from './apollo.config';
 
@@ -7,16 +8,19 @@ import { GraphQLModule } from './apollo.config';
 import { AppComponent } from './app.component';
 import { LinkItemComponent } from './link-item/link-item.component';
 import { LinkListComponent } from './link-list/link-list.component';
+import { CreateLinkComponent } from './create-link/create-link.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LinkItemComponent,
-    LinkListComponent
+    LinkListComponent,
+    CreateLinkComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
 
     // GraphQL connection
     GraphQLModule,
--- src/app/graphql.ts
+++ src/app/graphql.ts
@@ -22,3 +22,27 @@ export interface AllLinkQueryResponse {
   allLinks: Link[];
   // loading: boolean;  // Self NOTE: on tutorial seems that this was from an old version.
 }
+
+
+
+// 1
+export const CREATE_LINK_MUTATION = gql`
+  # 2
+  mutation CreateLinkMutation($description: String!, $url: String!) {
+    createLink(
+      description: $description,
+      url: $url,
+    ) {
+      id
+      createdAt
+      url
+      description
+    }
+  }
+`;
+
+//3
+export interface CreateLinkMutationResponse {
+  createLink: Link;
+  // loading: boolean;
+}
