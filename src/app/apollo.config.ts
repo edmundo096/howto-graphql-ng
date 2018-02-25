1f85dc003870772569b34943ed4482aa2e7716dc
Edmundo Elizondo <edmundo.geg@hotmail.com>
2018-02-25 02:22:02 -0600

    feat(app): Add apollo subscription dependencies and config

--- src/app/apollo.config.ts
+++ src/app/apollo.config.ts
@@ -3,26 +3,27 @@
  */
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HttpHeaders } from '@angular/common/http';
-// 1
 import { Apollo, ApolloModule } from 'apollo-angular';
 import { HttpLink, HttpLinkModule } from 'apollo-angular-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+
+import { getOperationAST } from 'graphql';
+import { WebSocketLink } from 'apollo-link-ws';
+import { ApolloLink } from 'apollo-link';
 
 import { GC_AUTH_TOKEN } from './constants';
 
 
 @NgModule({
   exports: [
-    // 2
     HttpClientModule,
     ApolloModule,
     HttpLinkModule
   ]
 })
 export class GraphQLModule {
-  // 3
-  constructor(apollo: Apollo, httpLink: HttpLink) {
 
+  constructor(apollo: Apollo, httpLink: HttpLink) {
     // Set headers for authorization.
     const token = localStorage.getItem(GC_AUTH_TOKEN);
     const authorization = token ? `Bearer ${token}` : null;
@@ -30,14 +31,33 @@ export class GraphQLModule {
     headers.append('Authorization', authorization);
     // TODO: Now we should configure the authorization rules (permissions) https://www.graph.cool/docs/reference/auth/authorization-iegoo0heez/
 
-    // 4
     const uri = 'https://api.graph.cool/simple/v1/cjdv3fspa0p8i010666pj8pif';
     const http = httpLink.create({ uri, headers });
 
-    // 6
+    // 1
+    const ws = new WebSocketLink({
+      uri: `wss://subscriptions.us-west-2.graph.cool/v1/cjdv3fspa0p8i010666pj8pif`,
+      options: {
+        reconnect: true,
+        connectionParams: {
+          authToken: localStorage.getItem(GC_AUTH_TOKEN),
+        }
+      }
+    });
+
     apollo.create({
-      link: http,
+      // 2
+      link: ApolloLink.split(
+        // 3
+        operation => {
+          const operationAST = getOperationAST(operation.query, operation.operationName);
+          return !!operationAST && operationAST.operation === 'subscription';
+        },
+        ws,
+        http,
+      ),
       cache: new InMemoryCache()
     });
   }
+
 }
